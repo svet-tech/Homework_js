b19ff272f36e1f2904f2845d7d19f1d21c8b92f2
svet-tech <svetla-gonchar@yandex.ru>
2025-01-16 22:05:19 +0300

    Update main.js

--- Work_3/3_5/main.js
+++ Work_3/3_5/main.js
@@ -1,27 +1,42 @@
-// function userNumbers() 
 let num = 0
 let i = 0
 let positiveNum = 0
 let negativeNum = 0
 let zero = 0
+let evenNum = 0
+let oddNum = 0
 
 do {
     num = prompt('Введите число')
-    i++
-    if (num > 0) {
+
+    if (num > 0 && num % 2 === 0) {
         positiveNum++
+        evenNum++
     }
-    else if (num < 0) {
+    else if (num > 0 && num % 2 != 0) {
+        positiveNum++
+        oddNum++
+    }
+    else if (num < 0 && num % 2 === 0) {
         negativeNum++
+        evenNum++
+    }
+    else if (num < 0 && num % 2 != 0) {
+        negativeNum++
+        oddNum++
     }
     else {
         zero++
     }
+
+    i++
 }
 while (i <= 10)
 
 console.log('Положительных чисел: ' + +positiveNum)
 console.log('Отрицательных чисел: ' + +negativeNum)
 console.log('Нолей: ' + +zero)
+console.log('Четных чисел: ' + +evenNum)
+console.log('Нечетных чисел: ' + +oddNum)
 
 
