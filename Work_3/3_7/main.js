d1101e3192255b49e44623e183d8d51c4764f3c5
svet-tech <svetla-gonchar@yandex.ru>
2025-01-18 06:49:06 +0300

    Add files via upload
    Adding work 3_7

---
+++ Work_3/3_7/main.js
@@ -0,0 +1,9 @@
+const numbers = prompt("")
+const sdvig = +prompt("")
+
+const srez = numbers.slice(0, sdvig)
+const srez2 = numbers.slice(sdvig, sdvig.lenght)
+console.log('srez1 =', srez)
+console.log('srez2 =', srez2)
+
+console.log('srez2 + srez', srez2 + srez)
